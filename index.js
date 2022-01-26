c994126e5c11d2ac221061290895e7a1f6cb9bca
Laney McGlohon <lmcglohon@gmail.com>
2022-01-26 07:40:37 -0800

    Simple server

---
+++ index.js
@@ -0,0 +1,26 @@
+// 1. Require apollo-server
+const { ApolloServer } = require('apollo-server')
+
+const typeDefs = `
+  type Query {
+    totalPhotos: Int!
+  }
+`
+
+const resolvers = {
+  Query: {
+    totalPhotos: () => 42
+  }
+}
+
+// 2. Create a new instance of the server
+// 3. Send it an object with typeDefs (the schema) and resolvers
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+// 4. Call listen on the server to launch the web server
+server
+  .listen()
+  .then(({url}) => console.log(`GraphQL Service running on ${url}`))
